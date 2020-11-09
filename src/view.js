af78338257c0dfac592eb18ae688eb5aa926a4a0
mongoose <crismsfiva@gmail.com>
2020-11-09 21:13:59 +0300

    add view file with watchers

---
+++ src/view.js
@@ -0,0 +1,22 @@
+import onChange from 'on-change';
+
+const inputField = document.querySelector('input');
+
+const renderError = (errors) => {
+  const error = errors;
+};
+
+export default (state) => onChange(state, (path, value) => {
+  switch (path) {
+    case 'form.processState':
+      console.log('state: ', state);
+      console.log('Обновился processState');
+      break;
+    case 'form.valid':
+      console.log('State ', state);
+      console.log('Валидность обновилась');
+      break;
+    default:
+      break;
+  }
+});
