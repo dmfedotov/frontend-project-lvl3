8eb8c9a0af2620944e9b351ff769a3f6f859b187
mongoose <crismsfiva@gmail.com>
2020-11-09 21:11:46 +0300

    add application file and import it

---
+++ src/app.js
@@ -0,0 +1,42 @@
+/* eslint-disable no-param-reassign  */
+
+import rss from './rss';
+import validate from './validate';
+import watcher from './view';
+
+const updateValidationState = (state) => validate.byUrl(state.form)
+  .then(({ url }) => rss.getData(url))
+  .then(() => {
+    state.form.valid = true;
+    state.form.errors = [];
+  })
+  .catch((err) => {
+    state.form.error = err.message;
+    state.form.valid = false;
+  });
+
+export default () => {
+  const state = {
+    form: {
+      processState: 'filling',
+      processError: null,
+      url: '',
+      valid: true,
+      error: null,
+    },
+    feeds: [],
+  };
+
+  const watchedState = watcher(state);
+
+  const form = document.querySelector('.rss-form');
+  const submitButton = form.querySelector('button');
+
+  form.addEventListener('submit', (evt) => {
+    evt.preventDefault();
+    const formData = new FormData(evt.target);
+    watchedState.form.url = formData.get('url');
+    watchedState.form.processState = 'sending';
+    updateValidationState(watchedState);
+  });
+};
--- src/index.js
+++ src/index.js
@@ -1,2 +1,6 @@
+import 'core-js';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/common.css';
+import app from './app';
+
+app();
