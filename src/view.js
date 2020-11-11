182bd5681a638cdb75a3cab8907737a56868ed75
mongoose <crismsfiva@gmail.com>
2020-11-11 17:58:40 +0300

    add render for feedback

--- src/view.js
+++ src/view.js
@@ -1,21 +1,63 @@
 import onChange from 'on-change';
 
-// const inputField = document.querySelector('input');
+const inputField = document.querySelector('input');
+const submitButton = document.querySelector('button');
+const feedbackElem = document.querySelector('.feedback');
 
-/* const renderError = (errors) => {
-  const error = errors;
+const renderSuccess = () => {
+  feedbackElem.classList.add('text-success');
+  feedbackElem.textContent = 'Rss has been loaded';
 };
-*/
 
-export default (state) => onChange(state, (path) => {
+const renderError = (error) => {
+  if (error) {
+    feedbackElem.classList.add('text-danger');
+    inputField.classList.add('border-danger');
+    feedbackElem.textContent = error;
+  }
+};
+
+const clearFeedback = () => {
+  inputField.classList.remove('border-danger');
+  feedbackElem.classList.remove('text-danger', 'text-success');
+  feedbackElem.textContent = '';
+};
+
+const formProcessStateHandler = (processState) => {
+  switch (processState) {
+    case 'filling':
+      inputField.disabled = false;
+      submitButton.disabled = false;
+      break;
+    case 'sending':
+      inputField.disabled = true;
+      submitButton.disabled = true;
+      clearFeedback();
+      break;
+    case 'failed':
+      inputField.disabled = false;
+      submitButton.disabled = false;
+      break;
+    case 'finished':
+      inputField.disabled = false;
+      inputField.value = '';
+      submitButton.disabled = false;
+      renderSuccess();
+      break;
+    default:
+      throw new Error(`Unknown state: ${processState}`);
+  }
+};
+
+export default (state) => onChange(state, (path, value) => {
   switch (path) {
     case 'form.processState':
-      console.log('state: ', state);
-      console.log('Обновился processState');
+      formProcessStateHandler(value);
       break;
     case 'form.valid':
-      console.log('State ', state);
-      console.log('Валидность обновилась');
+      break;
+    case 'form.error':
+      renderError(state.form.error);
       break;
     default:
       break;
