967f4acd847be7894cd7a7d9cfa5e517ad485cc6
mongoose <crismsfiva@gmail.com>
2020-11-17 17:19:16 +0300

    refactor view.js module

--- src/view.js
+++ src/view.js
@@ -2,25 +2,25 @@ import onChange from 'on-change';
 
 const inputField = document.querySelector('input');
 const submitButton = document.querySelector('button');
-const feedbackElem = document.querySelector('.feedback');
+const feedback = document.querySelector('.feedback');
 
 const renderSuccess = () => {
-  feedbackElem.classList.add('text-success');
-  feedbackElem.textContent = 'Rss has been loaded';
+  feedback.classList.add('text-success');
+  feedback.textContent = 'Rss has been loaded';
 };
 
 const renderError = (error) => {
   if (error) {
-    feedbackElem.classList.add('text-danger');
+    feedback.classList.add('text-danger');
     inputField.classList.add('border-danger');
-    feedbackElem.textContent = error;
+    feedback.textContent = error;
   }
 };
 
 const clearFeedback = () => {
   inputField.classList.remove('border-danger');
-  feedbackElem.classList.remove('text-danger', 'text-success');
-  feedbackElem.textContent = '';
+  feedback.classList.remove('text-danger', 'text-success');
+  feedback.textContent = '';
 };
 
 const formProcessStateHandler = (processState) => {
@@ -49,6 +49,39 @@ const formProcessStateHandler = (processState) => {
   }
 };
 
+const createLayout = () => {
+  const container = document.createElement('div');
+  container.classList.add('container-xl', 'feed-container');
+
+  const feedsBlock = document.createElement('div');
+  const postsBlock = document.createElement('div');
+  feedsBlock.classList.add('row');
+  postsBlock.classList.add('row');
+
+  const feedsWrapper = document.createElement('div');
+  const postsWrapper = document.createElement('div');
+  feedsWrapper.classList.add('col-md-10', 'mx-auto');
+  postsWrapper.classList.add('col-md-10', 'mx-auto');
+
+  const feedsTitle = document.createElement('h2');
+  feedsTitle.textContent = 'Feeds';
+  const postsTitle = document.createElement('h2');
+  postsTitle.textContent = 'Posts';
+
+  const feedsList = document.createElement('ul');
+  const postsList = document.createElement('ul');
+  feedsList.classList.add('list-group-5', 'feeds-list');
+  postsList.classList.add('list-group-5', 'posts-list');
+
+  feedsWrapper.append(feedsTitle, feedsList);
+  postsWrapper.append(postsTitle, postsList);
+  feedsBlock.append(feedsWrapper);
+  postsBlock.append(postsWrapper);
+  container.append(feedsBlock, postsBlock);
+
+  return container;
+};
+
 const renderPost = (list, posts) => {
   posts.forEach((post) => {
     const li = document.createElement('li');
@@ -64,35 +97,16 @@ const renderPost = (list, posts) => {
 const renderFeed = (feeds) => {
   const mainElem = document.querySelector('main');
   const containerElem = mainElem.querySelector('.feed-container');
+
   if (containerElem) {
     containerElem.remove();
   }
 
-  const container = document.createElement('div');
-  container.classList.add('container-xl', 'feed-container');
-  const feedsBlock = document.createElement('div');
-  const postsBlock = document.createElement('div');
-  feedsBlock.classList.add('row');
-  postsBlock.classList.add('row');
-  const feedsWrapper = document.createElement('div');
-  const postsWrapper = document.createElement('div');
-  feedsWrapper.classList.add('col-md-10', 'mx-auto');
-  postsWrapper.classList.add('col-md-10', 'mx-auto');
-  const feedsTitle = document.createElement('h2');
-  const postsTitle = document.createElement('h2');
-  const feedsList = document.createElement('ul');
-  const postsList = document.createElement('ul');
-  feedsList.classList.add('list-group-5');
-  postsList.classList.add('list-group-5');
-
-  feedsWrapper.append(feedsTitle, feedsList);
-  postsWrapper.append(postsTitle, postsList);
-  feedsBlock.append(feedsWrapper);
-  postsBlock.append(postsWrapper);
-  container.append(feedsBlock, postsBlock);
+  const container = createLayout();
+  const feedsList = container.querySelector('.feeds-list');
+  const postsList = container.querySelector('.posts-list');
 
   feeds.forEach((feed) => {
-    feedsTitle.textContent = 'Feeds';
     const title = document.createElement('h3');
     title.textContent = feed.title;
     const description = document.createElement('p');
@@ -101,6 +115,7 @@ const renderFeed = (feeds) => {
     li.classList.add('list-group-item');
     li.append(title, description);
     feedsList.append(li);
+
     renderPost(postsList, feed.posts);
   });
 
@@ -111,7 +126,6 @@ export default (state) => onChange(state, (path, value) => {
   switch (path) {
     case 'form.processState':
       formProcessStateHandler(value);
-      console.log(state);
       break;
     case 'form.valid':
       break;
