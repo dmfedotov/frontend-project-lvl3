7ed9469dd5e151c00d157050f1bcf1eb9c185052
mongoose <crismsfiva@gmail.com>
2020-11-17 17:16:57 +0300

    add function parse to parse rss document

--- src/rss.js
+++ src/rss.js
@@ -5,6 +5,25 @@ const getData = (url) => axios({
   timeout: 5000,
 }).then((response) => response.data.contents);
 
+const parse = (doc, url) => {
+  const channelElem = doc.querySelector('rss channel');
+  const title = channelElem.querySelector('title').textContent;
+  const description = channelElem.querySelector('description').textContent;
+  const postElems = channelElem.querySelectorAll('item');
+  const posts = Array.from(postElems).map((elem) => ({
+    title: elem.querySelector('title').textContent,
+    link: elem.querySelector('link').textContent,
+  }));
+
+  return {
+    url,
+    title,
+    description,
+    posts,
+  };
+};
+
 export default {
   getData,
+  parse,
 };
