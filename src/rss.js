5710c2805c7e93137ceb8f4b5c821a8c256530b8
mongoose <crismsfiva@gmail.com>
2020-11-19 17:31:16 +0300

    add identificators for feeds and posts

--- src/rss.js
+++ src/rss.js
@@ -1,16 +1,22 @@
 import axios from 'axios';
+import { uniqueId } from 'lodash';
 
 const getData = (url) => axios({
   url: `https://api.allorigins.win/get?url=${encodeURIComponent(url)}`,
   timeout: 5000,
 }).then((response) => response.data.contents);
 
+const generatePostId = (index) => index + 1;
+
 const parse = (doc, url) => {
+  const feedId = uniqueId();
   const channelElem = doc.querySelector('rss channel');
   const title = channelElem.querySelector('title').textContent;
   const description = channelElem.querySelector('description').textContent;
   const postElems = channelElem.querySelectorAll('item');
-  const posts = Array.from(postElems).map((elem) => ({
+  const posts = Array.from(postElems).map((elem, index) => ({
+    feedId,
+    id: generatePostId(index),
     title: elem.querySelector('title').textContent,
     link: elem.querySelector('link').textContent,
   }));
@@ -20,6 +26,7 @@ const parse = (doc, url) => {
     title,
     description,
     posts,
+    id: feedId,
   };
 };
 
