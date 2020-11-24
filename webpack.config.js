90d95e00d9c0aca341ab63074aab983d01b06f61
mongoose <crismsfiva@gmail.com>
2020-11-24 15:44:51 +0300

    modify webpack.config for using async/await

--- webpack.config.js
+++ webpack.config.js
@@ -31,6 +31,7 @@ module.exports = {
           loader: 'babel-loader',
           options: {
             presets: ['@babel/preset-env'],
+            plugins: ['@babel/plugin-transform-runtime'],
           },
         },
       },
