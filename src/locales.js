c84397f6fc6e20e591f3fe0d24fc330ffbf811dc
mongoose <crismsfiva@gmail.com>
2020-11-24 17:53:03 +0300

    add translation file for i18next

---
+++ src/locales.js
@@ -0,0 +1,15 @@
+export default {
+  en: {
+    translation: {
+      successMessage: 'Rss has been loaded',
+      titles: {
+        feeds: 'Feeds',
+        posts: 'Posts',
+      },
+      errors: {
+        duplicate: 'Rss already exists',
+        invalidContent: 'This source doesn\'t contain valid rss',
+      },
+    },
+  },
+};
