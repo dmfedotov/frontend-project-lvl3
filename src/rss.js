249f0a1a8d7491362e11c5911c6c2b0d0e0989e2
mongoose <crismsfiva@gmail.com>
2020-11-30 18:20:15 +0300

    refactor parser for correct errors processing

--- src/rss.js
+++ src/rss.js
@@ -1,3 +1,4 @@
+import i18next from 'i18next';
 import axios from 'axios';
 import { uniqueId } from 'lodash';
 
@@ -9,25 +10,33 @@ const getData = (url) => axios({
 const generatePostId = (index) => index + 1;
 
 const parse = (doc, url) => {
-  const feedId = uniqueId();
-  const channelElem = doc.querySelector('rss channel');
-  const title = channelElem.querySelector('title').textContent;
-  const description = channelElem.querySelector('description').textContent;
-  const postElems = channelElem.querySelectorAll('item');
-  const posts = Array.from(postElems).map((elem, index) => ({
-    feedId,
-    id: generatePostId(index),
-    title: elem.querySelector('title').textContent,
-    link: elem.querySelector('link').textContent,
-  }));
+  const promise = Promise.resolve();
+  return promise
+    .then(() => {
+      const feedId = uniqueId();
+      const channelElem = doc.querySelector('rss channel');
+      const title = channelElem.querySelector('title').textContent;
+      const description = channelElem.querySelector('description').textContent;
+      const postElems = channelElem.querySelectorAll('item');
+      const posts = Array.from(postElems).map((elem, index) => ({
+        feedId,
+        id: generatePostId(index),
+        title: elem.querySelector('title').textContent,
+        link: elem.querySelector('link').textContent,
+      }));
 
-  return {
-    url,
-    title,
-    description,
-    posts,
-    id: feedId,
-  };
+      return {
+        url,
+        title,
+        description,
+        posts,
+        id: feedId,
+      };
+    })
+    .catch((err) => {
+      console.log(err);
+      throw new Error(i18next.t('errors.unexpectedBehavior'));
+    });
 };
 
 export default {
