bfc414eadad815a22203849eee7456fc434cc0c3
mongoose <crismsfiva@gmail.com>
2021-01-17 14:03:17 +0300

    simplify rss module making it a parse module

--- src/parse.js
+++ src/parse.js
@@ -1,21 +1,13 @@
 /* eslint-disable no-param-reassign */
 
-import axios from 'axios';
-import { differenceBy, uniqueId } from 'lodash';
-
-const getProxyUrl = (url) => `https://api.allorigins.win/get?url=${encodeURIComponent(url)}`;
-
-const getData = (url) => axios({
-  url: getProxyUrl(url),
-  timeout: 5000,
-}).then((response) => response.data.contents);
+import { uniqueId } from 'lodash';
 
 const parse = (data) => {
   const parser = new DOMParser();
   const doc = parser.parseFromString(data, 'text/xml');
   const error = doc.querySelector('parsererror');
   if (error) {
-    throw new Error('errors.invalidContent');
+    throw new Error('errors.parsing');
   }
 
   const channelElem = doc.querySelector('rss channel');
@@ -32,8 +24,6 @@ const parse = (data) => {
   return { title, description, posts };
 };
 
-const findNewPosts = (currentPosts, updatedPosts) => differenceBy(updatedPosts, currentPosts, 'link');
-
 const buildFeed = (data, url, id) => {
   const feed = { ...data, url, id: String(id) };
   return feed;
@@ -47,32 +37,8 @@ const buildPosts = (data, feedId) => {
   return posts;
 };
 
-const autoupdate = (state) => setTimeout(() => {
-  const urls = state.feeds.map(({ url }) => url);
-
-  const promises = urls.map((url, index) => {
-    const data = getData(url, state);
-    const feedId = String(index + 1);
-    return data
-      .then((content) => {
-        const feedData = parse(content);
-        return buildPosts(feedData.posts, feedId);
-      });
-  });
-
-  return Promise.all(promises)
-    .then(([updatedPosts]) => {
-      const newPosts = findNewPosts(state.posts, updatedPosts);
-      state.posts.unshift(...newPosts);
-    })
-    .finally(() => autoupdate(state))
-    .catch(console.log);
-}, 5000);
-
 export {
-  getData,
   parse,
   buildFeed,
   buildPosts,
-  autoupdate,
 };
